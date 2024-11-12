a827a0a10baa9ad9f0bf2fae5f5dd8a932c9009f
neil-jay <neiljay@pcmender.online>
2024-11-12 11:53:09 +0530

    Added light-dark mode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export function Footer() {
   return (
-    <footer className="border-t bg-background">
+    <footer className="border-t bg-background text-foreground">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left">
@@ -15,7 +15,7 @@ export function Footer() {
             >
               Your Name
             </a>
-            .
+            . 
           </p>
         </div>
         <p className="text-center text-sm md:text-left">
